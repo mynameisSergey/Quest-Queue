22aa1faf0fd591e6bc2aff3d015cdf06403db01a
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-28 01:14:00 +0300

    fix: добавлена проверка на null

--- src/adapters/InstantAdapter.java
+++ src/adapters/InstantAdapter.java
@@ -10,11 +10,18 @@ import java.time.Instant;
 public class InstantAdapter extends TypeAdapter<Instant> {
     @Override
     public void write(JsonWriter jsonWriter, Instant instant) throws IOException {
-        jsonWriter.value(instant.toEpochMilli());
+        if (instant == null)
+            jsonWriter.nullValue(); // Обработка null
+        else
+            jsonWriter.value(instant.toEpochMilli());
     }
 
     @Override
     public Instant read(JsonReader jsonReader) throws IOException {
-        return Instant.ofEpochMilli(Long.parseLong(jsonReader.nextString()));
+        if (jsonReader.peek() == com.google.gson.stream.JsonToken.NULL) {
+            jsonReader.nextNull(); // Обработка null
+            return null;
+        }
+        return Instant.ofEpochMilli(jsonReader.nextLong()); // Используйте nextLong()
     }
 }
