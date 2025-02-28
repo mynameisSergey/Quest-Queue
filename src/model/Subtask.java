587cca882d50edcf7b0fe6f952333ff4133859a8
Sergey <116033520+mynameisSergey@users.noreply.github.com>
2025-02-28 09:42:13 +0300

    feat: добавить метод validateEpicId в модель Subtask

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -5,29 +5,34 @@ import java.util.Objects;
 public class Subtask extends Task {
     private int epicId;
 
-
     public Subtask(TasksType type, String title, String description, Instant startTime,
                    long duration,int epicId) {
         super(type, title, description, startTime, duration);
+        validateEpicId(epicId);
         this.epicId = epicId;
     }
 
     public Subtask(TasksType type, String title, String description, StatusTasks statusTasks, Instant startTime,
                    long duration,int epicId) {
         super(type, title, description, statusTasks, startTime, duration);
+        validateEpicId(epicId);
         this.epicId = epicId;
     }
 
     public Subtask(int id, TasksType type, String title, String description, StatusTasks statusTasks, Instant startTime,
                    long duration, int epicId) {
         super(id, type, title, description, statusTasks,startTime,duration);
+        validateEpicId(epicId);
         this.epicId = epicId;
     }
-
+    private void validateEpicId(int epicId) {
+        if (epicId < 0) {
+            throw new IllegalArgumentException("Epic ID cannot be negative");
+        }
+    }
     public int getEpicId() {
         return epicId;
     }
-
 
     @Override
     public boolean equals(Object o) {
